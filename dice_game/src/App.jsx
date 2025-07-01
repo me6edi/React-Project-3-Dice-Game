2c147aafb4f6e9a35422d7f7198070b4d86af347
me6edi <exited17@gmail.com>
2025-07-01 11:03:21 +0600

    Dice Game Complited

--- dice_game/src/App.jsx
+++ dice_game/src/App.jsx
@@ -1,22 +1,17 @@
-import "./App.css";
-import StartGame from './components/StartGame';
+import { useState } from "react";
+import StartGame from "./components/StartGame";
+import GamePlay from "./components/GamePlay";
 
 function App() {
-
-  const [isGameStarted,setIsGameStarted] = useState(false)
+  const [isGameStarted, setIsGameStarted] = useState(false);
 
   const toggleGamePlay = () => {
-
     setIsGameStarted((prev) => !prev);
   };
+
   return (
-    <div>
-      <StartGame />
-    </div>
+    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
   );
 }
 
 export default App;
-
-
-
--- dice_game/src/components/StartGame.jsx
+++ dice_game/src/components/StartGame.jsx
@@ -1,19 +1,21 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { Button } from "../styled/Button";
 
-const StartGame = () => {
-  return <Container>
-    <div>
-          <img src="img/dices.png" alt="dices" />
-    </div>
+const StartGame = ({ toggle }) => {
+  return (
+    <Container>
+      <div>
+        <img src="/images/dices.png" />
+      </div>
+      <div className="content">
+        <h1>Dice Game</h1>
+        <Button onClick={toggle}>Play Now</Button>
+      </div>
+    </Container>
+  );
+};
 
-    <div className="content">
-          <h1>Dice Game</h1>
-          <Button>Play Now</Button>
-    </div>
-  </Container>
-}
-
-export default StartGame
+export default StartGame;
 
 const Container = styled.div`
   max-width: 1180px;
@@ -22,25 +24,10 @@ const Container = styled.div`
   margin: 0 auto;
   align-items: center;
 
-  .content{
-  h1{
-    font-size:96px;
+  .content {
+    h1 {
+      font-size: 96px;
+      white-space: nowrap;
+    }
   }
-}
-  `;
-const Button = styled.button`
-      color:white;
-      padding:10px 18px;
-      background:#000000;
-      border-radius:5px;
-      min-width:220px;
-      border:1px solid transparent;
-      cursor:pointer;
-
-      &:hover{
-      background-color:white;
-      border:1px solid;
-      color:black;
-      transition:0.ss background ease-in;
-      }
 `;
